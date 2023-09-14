06fcc88c4f64deefb7464357e4bc94273d59ed19
stayfocus06 <139497243+stayfocus06@users.noreply.github.com>
2023-09-14 10:57:44 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
   name               = "raham-terraform-elb"
-  availability_zones = ["eu-west-3a", "eu-west-3b"]
+  availability_zones = ["us-east-1a", "us-east-1b"]
 
   listener {
     instance_port     = 80
@@ -31,15 +31,15 @@ resource "aws_elb" "bar" {
   engine_version          = "5.7.mysql_aurora.2.11.2"
   availability_zones      = ["eu-west-3a", "eu-west-3b"]
   database_name           = "mydb"
-  master_username         = "raham"
-  master_password         = "Rahamshaik#444555"
+  master_username         = "suryank"
+  master_password         = "suryank@123"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
 }*/
 
 /*resource "aws_instance" "one" {
   for_each = toset(["one", "two", "three"])
-  ami           = "ami-006be9ab6a140de6e"
+  ami           = "ami-0f844a9675b22ea32"
   instance_type = "t2.micro"
   tags = {
     Name = "instance-${each.key}"
